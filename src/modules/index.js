41a8b60149f25c4d00f2e76890b0a973a5fb4eab
Antosik <grig_anton96@mail.ru>
2017-06-25 12:08:12 +0300

    [mod] rework modules index

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,4 +1,28 @@
+import Promise from 'bluebird';
+
 import pixiv from './pixiv';
 import twitter from './twitter';
 
-export { pixiv, twitter };
+function getImages({ type, link, all = false }) {
+  switch (type) {
+    case 'Twitter':
+      return twitter.getImages(link);
+    case 'Pixiv':
+      return pixiv.getImages(link, all);
+    default:
+      return Promise.reject('Module not found!');
+  }
+}
+
+function downloadImage({ type, path }, image) {
+  switch (type) {
+    case 'Twitter':
+      return twitter.downloadImage(image, path);
+    case 'Pixiv':
+      return pixiv.downloadImage(image, path);
+    default:
+      return Promise.reject('Module not found!');
+  }
+}
+
+export { getImages, downloadImage };
