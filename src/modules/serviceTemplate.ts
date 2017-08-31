437cdbc5c8514803f72ff91d38d87e04952375e1
Antosik <grig_anton96@mail.ru>
2017-08-31 13:38:17 +0300

    [modules] service module realisation template

---
+++ src/modules/serviceTemplate.ts
@@ -0,0 +1,55 @@
+import * as bluebirdPromise from 'bluebird';
+import { EventEmitter } from 'events';
+
+export enum IQuestionTypes {
+    list = 'list',
+    confirm = 'confirm',
+    input = 'input',
+    password = 'password',
+}
+
+export interface IQuestion {
+    name: string;
+    type: IQuestionTypes;
+    message: any;
+    when?(answers: any): boolean;
+    validate?(input: string, hash: any): boolean;
+}
+
+export interface IService {
+    serviceName: string;
+    serviceLink: string;
+    questions: IQuestion[];
+
+    regExpLink?: RegExp;
+    validateLink?(link: string): boolean;
+    search(link: string, options: any): IServiceSearch;
+}
+
+export abstract class IServiceSearch {
+    public resource: string;
+    public filepath: string;
+    public events: EventEmitter;
+    public options: any;
+
+    public images: string[];
+
+    public constructor(public data: string, options: any = { path: 'images', all: true }) {
+        this.resource = data;
+        this.options = options;
+        this.filepath = options.path;
+        this.events = new EventEmitter();
+        this.images = [];
+    }
+
+    public abstract async getImages(): Promise<string[]>;
+    public abstract async downloadImage(url: string,  index: number): Promise<void>;
+    public async downloadImages(): Promise<void> {
+        bluebirdPromise
+            .resolve(this.images)
+            .map(async (url, index) => {
+                await this.downloadImage(url, index);
+            }, { concurrency: 5 });
+        return;
+    }
+}
