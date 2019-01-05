d8b64a51811631ce98654a6c88f80cf31d088c24
Antosik <grig_anton96@mail.ru>
2019-01-05 20:15:56 +0300

    [test][mod] update pixiv tests

--- test/modules/pixiv.test.ts
+++ test/modules/pixiv.test.ts
@@ -1,61 +1,105 @@
 import { pixivRegExp, PixivSearch } from "../../src";
 
-test("init", () => {
-  expect.assertions(3);
-  const search = new PixivSearch({});
-  expect(search).not.toBeNull();
-  expect(search).toBeDefined();
-  expect(search instanceof PixivSearch).toBeTruthy();
-});
+describe("pixiv tests", () => {
+  test("init", () => {
+    expect.assertions(3);
+    const search = new PixivSearch({});
+    expect(search).not.toBeNull();
+    expect(search).toBeDefined();
+    expect(search instanceof PixivSearch).toBeTruthy();
+  });
 
-test("login", async () => {
-  expect.assertions(1);
-  const search = new PixivSearch({});
-  const authorized = await search.login();
-  expect(authorized).toBeFalsy();
-});
+  test("login", async () => {
+    expect.assertions(1);
+    const search = new PixivSearch({});
+    const authorized = await search.login();
+    expect(authorized).toBeFalsy();
+  });
 
-test("login with invalid credentials", async () => {
-  expect.assertions(1);
-  const search = new PixivSearch({ username: "username", password: "password" });
-  const authorized = await search.login();
-  expect(authorized).toBeFalsy();
-});
+  describe("links", () => {
+    test("valid links", async () => {
+      expect.assertions(1);
+      expect(
+        pixivRegExp.test("https://www.pixiv.net/member_illust.php?id=10655554")
+      ).toBeTruthy();
+    });
 
-test("login with valid credentials", async () => {
-  expect.assertions(1);
-  const search = new PixivSearch({ username: process.env.PIXIV_USERNAME, password: process.env.PIXIV_PASSWORD });
-  const authorized = await search.login();
-  expect(authorized).toBeTruthy();
-}, 60000);
+    test("invalid links", async () => {
+      expect.assertions(4);
+      expect(pixivRegExp.test("https://google.com")).toBeFalsy();
+      expect(
+        pixivRegExp.test("https://www.pixiv.nets/member_illust.php?id=10655554")
+      ).toBeFalsy();
+      expect(pixivRegExp.test("https://twitter.com/genskc")).toBeFalsy();
+      expect(pixivRegExp.test("https://www.deviantart.com/kvacm")).toBeFalsy();
+    });
+  });
 
-test("valid links", async () => {
-  expect.assertions(1);
-  expect(pixivRegExp.test("https://www.pixiv.net/member_illust.php?id=10655554")).toBeTruthy();
-});
+  describe("unauthorized session", () => {
+    const search = new PixivSearch({
+      username: "username",
+      password: "password"
+    });
 
-test("invalid links", async () => {
-  expect.assertions(4);
-  expect(pixivRegExp.test("https://google.com")).toBeFalsy();
-  expect(pixivRegExp.test("https://www.pixiv.nets/member_illust.php?id=10655554")).toBeFalsy();
-  expect(pixivRegExp.test("https://twitter.com/genskc")).toBeFalsy();
-  expect(pixivRegExp.test("https://www.deviantart.com/kvacm")).toBeFalsy();
-});
+    test("login with invalid credentials", async () => {
+      expect.assertions(1);
+      const authorized = await search.login();
+      expect(authorized).toBeFalsy();
+    });
 
-test("getting images", async () => {
-  expect.assertions(2);
-  const search = new PixivSearch({ username: process.env.PIXIV_USERNAME, password: process.env.PIXIV_PASSWORD });
-  const images = await search.getImages("https://www.pixiv.net/member_illust.php?id=10655554");
-  expect(Array.isArray(images)).toBeTruthy();
-  expect(images.length).toBeGreaterThan(0);
-}, 60000);
+    test("getting images with valid link", async () => {
+      expect.assertions(1);
+      try {
+        await search.getImages(
+          "https://www.pixiv.net/member_illust.php?id=10655554"
+        );
+      } catch (e) {
+        expect(e.message).toMatch(/Pixiv account credentials need!/);
+      }
+    }, 60000);
 
-test("getting images failed", async () => {
-  expect.assertions(1);
-  const search = new PixivSearch({ username: process.env.PIXIV_USERNAME, password: process.env.PIXIV_PASSWORD });
-  try {
-    await search.getImages("https://www.pixiv.nets/member_illust.php?id=10655554");
-  } catch (e) {
-    expect(e.message).toMatch(/Invalid pixiv link/);
-  }
+    test("getting images with invalid link", async () => {
+      expect.assertions(1);
+      try {
+        await search.getImages(
+          "https://www.pixiv.nets/member_illust.php?id=10655554"
+        );
+      } catch (e) {
+        expect(e.message).toMatch(/Invalid pixiv link/);
+      }
+    });
+  });
+
+  describe("authorized session", () => {
+    const search = new PixivSearch({
+      username: process.env.PIXIV_USERNAME,
+      password: process.env.PIXIV_PASSWORD
+    });
+
+    test("login with valid credentials", async () => {
+      expect.assertions(1);
+      const authorized = await search.login();
+      expect(authorized).toBeTruthy();
+    }, 60000);
+
+    test("getting images with valid link", async () => {
+      expect.assertions(2);
+      const images = await search.getImages(
+        "https://www.pixiv.net/member_illust.php?id=10655554"
+      );
+      expect(Array.isArray(images)).toBeTruthy();
+      expect(images.length).toBeGreaterThan(0);
+    }, 60000);
+
+    test("getting images with invalid link", async () => {
+      expect.assertions(1);
+      try {
+        await search.getImages(
+          "https://www.pixiv.nets/member_illust.php?id=10655554"
+        );
+      } catch (e) {
+        expect(e.message).toMatch(/Invalid pixiv link/);
+      }
+    });
+  });
 });
