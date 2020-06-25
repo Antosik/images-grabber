e0373b6bd4c292cbb8ec76b567008280278b4c4f
Antosik <grig_anton96@mail.ru>
2020-06-26 00:30:32 +0300

    3.1.4

--- src/modules/deviantart.ts
+++ src/modules/deviantart.ts
@@ -1,7 +1,6 @@
 import co from "co";
 import { extname } from "path";
 import { URL } from "url";
-import { promisify } from "util";
 import * as xml2js from "xml2js";
 
 import AServiceSearch from "../types/AServiceSearch";
@@ -9,12 +8,10 @@ import { req, wait, writeBuffer } from "../util/functions";
 
 class DeviantartSearch extends AServiceSearch {
   private readonly parser: xml2js.Parser;
-  private readonly parseXML;
 
   constructor(options) {
     super(options);
     this.parser = new xml2js.Parser();
-    this.parseXML = promisify(this.parser.parseString);
   }
 
   /**
@@ -42,16 +39,12 @@ class DeviantartSearch extends AServiceSearch {
    * @param path Path to images folder
    * @param index Index of image
    */
-  public async downloadImage(
-    url: string,
-    path: string,
-    index: number
-  ): Promise<void> {
+  public async downloadImage(url: string, path: string, index: number): Promise<void> {
     const pathname = new URL(url).pathname;
     const file = `${path}/${index}${extname(pathname)}`;
 
-    await req(url, { encoding: null }) // tslint:disable-line no-null-keyword
-      .then(data => writeBuffer(file, data))
+    await req(url, { responseType: "arraybuffer" })
+      .then(({ data }) => writeBuffer(file, data))
       .catch(e =>
         this.events.emit("error", `Image (${url}) downloading error: ${e}`)
       );
@@ -72,18 +65,24 @@ class DeviantartSearch extends AServiceSearch {
     return authorID;
   }
 
-  private mediaReq(authorID: string, offset = 0): Promise<string> {
+  private async mediaReq(authorID: string, offset = 0): Promise<any> {
     return req(
-      `https://backend.deviantart.com/rss.xml?type=deviation&q=by%3A${authorID}+sort%3Atime+meta%3Aall&offset=${offset}`
-    ).catch(err => {
-      this.events.emit("error", `DeviantArt request error: ${err}`);
-      return "";
-    });
+      `https://backend.deviantart.com/rss.xml?type=deviation&q=by%3A${authorID}+sort%3Atime+meta%3Aall&offset=${offset}`,
+      { responseType: "text" }
+    )
+      .then(({ data }) => data)
+      .catch(err => {
+        this.events.emit("error", `DeviantArt request error: ${err}`);
+        return "";
+      });
   }
 
   private getMedia(json): string[] {
     const { unsafe } = this.options;
-    return json.rss.channel[0].item
+
+    const items = json?.rss?.channel[0]?.item ?? [];
+
+    return items
       .map(el => {
         if (el["media:rating"][0] === "adult") {
           if (unsafe) {
@@ -98,43 +97,50 @@ class DeviantartSearch extends AServiceSearch {
   }
 
   private hasNextPage(json): boolean {
+    const atomLinks = json?.rss?.channel[0]["atom:link"] ?? [];
+    if (atomLinks.length === 0) {
+      return false;
+    }
+
     return (
-      (!!json.rss.channel[0]["atom:link"][1] &&
-        json.rss.channel[0]["atom:link"][1].$.rel === "next") ||
-      (!!json.rss.channel[0]["atom:link"][2] &&
-        json.rss.channel[0]["atom:link"][2].$.rel === "next")
+      (Boolean(atomLinks[1]) && atomLinks[1].$.rel === "next")
+      || (Boolean(atomLinks[2]) && atomLinks[2].$.rel === "next")
     );
   }
 
   private getImagesCount(json): number {
-    return json.rss.channel[0].item.length;
+    return json?.rss?.channel[0]?.item.length;
   }
 
   private async getPage(authorID: string, offset = 0) {
     const xml = await this.mediaReq(authorID, offset);
-    const data = await this.parseXML(xml);
+    const data = await this.parser.parseStringPromise(xml);
 
     const nextPage = this.hasNextPage(data);
-    const count = this.getImagesCount(data) || 0;
-    const images = this.getMedia(data) || [];
+    const count = this.getImagesCount(data) ?? 0;
+    const images = this.getMedia(data) ?? [];
 
     return { nextPage, count, images };
   }
 
   private *getIllusts(authorID: string) {
     let json = yield this.getPage(authorID);
+
     let count = json.count;
     let results = json.images;
+
     this.events.emit("findImages", results.length);
 
     while (json.nextPage) {
       json = yield this.getPage(authorID, count);
+
       count += json.count;
       results = results.concat(json.images);
+
       this.events.emit("findImages", results.length);
     }
-    this.events.emit("findImages", results.length);
 
+    this.events.emit("findImages", results.length);
     return results;
   }
 }
